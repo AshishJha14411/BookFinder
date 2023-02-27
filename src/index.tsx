92e59fcbd60078f47b30812e3c7ca466f68205f3
Ashish Jha <ashishjha14411@gmail.com>
2023-02-27 12:06:58 +0530

    wrapped contexts around

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BookListProvider } from './context/BookSearchContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
+  <BookListProvider>
     <App />
-  </React.StrictMode>
+  </BookListProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
