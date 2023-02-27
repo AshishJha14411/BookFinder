01255dff58d52741990efce9e096456e3069cf13
Ashish Jha <ashishjha14411@gmail.com>
2023-02-27 11:51:31 +0530

    added comments

--- src/context/BookPageContext.tsx
+++ src/context/BookPageContext.tsx
@@ -1,21 +1,23 @@
 import { createContext, useEffect, useState, useContext } from "react";
 import { BookPageType, BookData } from './types';
-import axios from "axios";
-
 import { BookContext } from '../context/types';
 import { BookList } from '../context/BookSearchContext';
 
 export const BookPageContext = createContext<BookPageType|null>(null)
 
 export const BookPageProvider = (props:any) => {
-    
+    /* Importing list of books from Booklist context */
   const { bookList } = useContext(BookList) as BookContext
+  /* State for storing one book data */
     const [bookPageData, setBookPageData] = useState<BookData[]>([])
+    /* State for storing ID of the book clicked  */
     const [bookId, setBookId] = useState<string>("")
+    /* Filter based on selected ID */
     const fitlerFunc = (id:string) => {
         const temp = bookList.filter((item) => item.id===id)
         setBookPageData(temp)
     }
+    /* Run FilterFunc every BookId changes */
     useEffect(() => {
         fitlerFunc(bookId)
         console.log(bookPageData)
--- src/context/BookSearchContext.tsx
+++ src/context/BookSearchContext.tsx
@@ -1,33 +1,36 @@
-import { createContext, useEffect, useState, useContext } from 'react';
-import { BookContext, BookData, BookPageType } from './types';
+import { createContext, useEffect, useState } from 'react';
+import { BookContext, BookData } from './types';
 import axios from "axios";
-import { BookPageContext } from './BookPageContext';
-
 
 export const BookList = createContext<BookContext|null>(null)
 
-export const BookListProvider = (props:any) => {/* 
-  const {bookPageData, setBookId} = useContext(BookPageContext) as BookPageType */
+export const BookListProvider = (props:any) => {
+  /* State for storing search list */
     const [bookList, setBookList] = useState<BookData[]>([])
+    /* State for Conditionally rendering Home text and img */
     const [showHome, setShowHome] = useState(true)
+    /* State for storing search string */
     const [query, setQuery] = useState<string>("")
+    /* State for search init condition */
     const [search, setSearch] = useState<boolean>(false)
 
+    /* Fetching the Booklist from api */
     const calldata = async () => {
+      /* Api key */
         const apiKey: string | undefined = process.env.REACT_APP_API_KEY
         const response = await axios.get(
           `https://www.googleapis.com/books/v1/volumes?q={${query}&${apiKey}}`
         );
         setBookList(response.data.items);
-        /* setBookId("") */
       };
-      
+      /* post get req if "Search" is true and reseting search and query */
       if(search){
         calldata()
         console.log(bookList)
         setSearch(false)
         setQuery("")
       }
+      /* Initial One time run */
       useEffect(() => {
         calldata()
         console.log(bookList)
